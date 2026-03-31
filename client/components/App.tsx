72c8ed93bdc4ff80869bf4699377433d30b19146
Patrick Hignell <patrickhignell@hotmail.com>
2026-03-31 21:08:46 +1300

    add hide options

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { DiceIcon, OptionType } from "../../models/main"
 import Dice from "./Dice"
 import Select, { SingleValue } from 'react-select'
@@ -7,6 +7,9 @@ function App() {
   const [key, setKey] = useState<OptionType>({ label: "2D6", value: { dice: "D6", number: 2 } })
   const [turns, setTurns] = useState<number>(0)
   const [message, setMessage] = useState<string>("")
+  const [showOuter, setShowOuter] = useState<boolean>(true)
+  const [showInner, setShowInner] = useState<boolean>(true)
+  const [hideNotKey,setHideNotKey] = useState<boolean>(false)
   const outerWheel: DiceIcon[] = [
 {number: 1, dice: 'D4' },
 {number: 10, dice: 'D12' },
@@ -35,34 +38,7 @@ function App() {
 {number: 4, dice: 'D10' },
 {number: 9, dice: 'D12'}
   ]
-//   [
-// {number: 1, dice: 'D4'},
-// {number: 2, dice: 'D4'},
-// {number: 3, dice: 'D4'},
-// {number: 4, dice: 'D4'},
-// {number: 1, dice: 'D10'},
-// {number: 2, dice: 'D10'},
-// {number: 3, dice: 'D10'}, 
-// {number: 4, dice: 'D10'},
-// {number: 5, dice: 'D10'}, 
-// {number: 6, dice: 'D10'},
-// {number: 7, dice: 'D10'},
-// {number: 8, dice: 'D10'},
-// {number: 9, dice: 'D10'},
-// {number: 10, dice: 'D10'}, 
-// {number: 1, dice: 'D12'},
-// {number: 2, dice: 'D12'},
-// {number: 3, dice: 'D12'},
-// {number: 4, dice: 'D12'},
-// {number: 5, dice: 'D12'},
-// {number: 6, dice: 'D12'},
-// {number: 7, dice: 'D12'},
-// {number: 8, dice: 'D12'},
-// {number: 9, dice: 'D12'},
-// {number: 10, dice: 'D12'},
-// {number: 11, dice: 'D12'}, 
-// {number: 12, dice: 'D12'}
-//   ]
+
   const innerWheel: DiceIcon[] = [
     { number: 2, dice: 'D6' },
     { number: 11, dice: 'D20' }, 
@@ -92,90 +68,6 @@ function App() {
     { number: 10, dice: 'D20' }
   ]
 
-//   [
-//     { number: 5, dice: 'D6' },
-// {number: "L", dice:'Blank'},
-//     { number: 1, dice: 'D20' }, 
-// {number: "M", dice:'Blank'},
-//     { number: 2, dice: 'D20' },
-// {number: "N", dice:'Blank'},
-//     { number: 3, dice: 'D20' }, 
-// {number: "O", dice:'Blank'},
-//     { number: 4, dice: 'D20' },
-// {number: "P", dice:'Blank'},
-//     { number: 6, dice: 'D6' },
-// {number: "Q", dice:'Blank'},
-//     { number: 5, dice: 'D20' },
-// {number: "R", dice:'Blank'},
-//     { number: 6, dice: 'D20' },
-// {number: "S", dice:'Blank'},
-//     { number: 7, dice: 'D20' },
-// {number: "T", dice:'Blank'},
-//     { number: 1, dice: 'D6' },
-// {number: "U", dice:'Blank'},
-//     { number: 8, dice: 'D20' },
-// {number: "V", dice:'Blank'},
-//     { number: 9, dice: 'D20' },
-// {number: "W", dice:'Blank'},
-//     { number: 10, dice: 'D20' }, 
-// {number: "X", dice:'Blank'},
-//     { number: 2, dice: 'D6' },
-// {number: "Y", dice:'Blank'},
-//     { number: 11, dice: 'D20' }, 
-// {number: "Z", dice:'Blank'},
-//     { number: 12, dice: 'D20' },
-// {number: "A", dice:'Blank'},
-//     { number: 13, dice: 'D20' }, 
-// {number: "B", dice:'Blank'},
-//     { number: 14, dice: 'D20' },
-// {number: "C", dice:'Blank'},
-//     { number: 3, dice: 'D6' },
-// {number: "D", dice:'Blank'},
-//     { number: 15, dice: 'D20' },
-// {number: "E", dice:'Blank'},
-//     { number: 16, dice: 'D20' }, 
-// {number: "F", dice:'Blank'},
-//     { number: 17, dice: 'D20' },
-// {number: "G", dice:'Blank'},
-//     { number: 4, dice: 'D6' },
-// {number: "H", dice:'Blank'},
-//     { number: 18, dice: 'D20' },
-// {number: "I", dice:'Blank'},
-//     { number: 19, dice: 'D20' },
-// {number: "J", dice:'Blank'},
-//     { number: 20, dice: 'D20' },
-// {number: "K", dice:'Blank'}
-//   ]
-
-//   [
-// {number: 1, dice: 'D6'},
-// {number: 2, dice: 'D6'},
-// {number: 3, dice: 'D6'},
-// {number: 4, dice: 'D6'},
-// {number: 5, dice: 'D6'},
-// {number: 6, dice: 'D6'},
-// {number: 1, dice: 'D20'}, 
-// {number: 2, dice: 'D20'},
-// {number: 3, dice: 'D20'}, 
-// {number: 4, dice: 'D20'},
-// {number: 5, dice: 'D20'},
-// {number: 6, dice: 'D20'},
-// {number: 7, dice: 'D20'},
-// {number: 8, dice: 'D20'},
-// {number: 9, dice: 'D20'},
-// {number: 10, dice: 'D20'}, 
-// {number: 11, dice: 'D20'}, 
-// {number: 12, dice: 'D20'},
-// {number: 13, dice: 'D20'}, 
-// {number: 14, dice: 'D20'},
-// {number: 15, dice: 'D20'},
-// {number: 16, dice: 'D20'}, 
-// {number: 17, dice: 'D20'},
-// {number: 18, dice: 'D20'},
-// {number: 19, dice: 'D20'},
-// {number: 20, dice: 'D20'}
-  //   ]
-  
   const letterWheel = [
 {number: "A", dice:'Blank'}, 
 {number: "B", dice:'Blank'},
@@ -221,20 +113,26 @@ function App() {
 
   useEffect(() => {
     setTurns(innerWheel.findIndex((dice) => dice.number == key.value.number && dice.dice == key.value.dice))
-    console.log(turns)
   }, [key])
-
-  const showInner = true
-  const showOuter = true
 
   function handleKeyChange(e: SingleValue<OptionType>) {
     if (e) setKey(e)
-    console.log(e)
   }
 
   function handleMessageChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
     setMessage(event.target.value.toUpperCase());
   };
+  const handleShowOuter = (e: ChangeEvent<HTMLInputElement>) => {
+    setShowOuter(e.target.checked);
+  }
+
+  const handleShowInner = (e: ChangeEvent<HTMLInputElement>) => {
+    setShowInner(e.target.checked);
+  }
+
+  const handleHideNotKey = (e: ChangeEvent<HTMLInputElement>) => {
+    setHideNotKey(e.target.checked);
+  }
 
   return (
       <div>
@@ -263,7 +161,7 @@ function App() {
             transform: `rotate(${(i / innerWheel.length) * 360 + (innerOffset*innerTurns)}deg) translateY(-${innerRadius}px)`,
           }}
         >
-          <Dice {...dice} />
+            <Dice {...dice} hideNotKey={dice.dice !=key.value.dice || dice.number !=key.value.number ? hideNotKey : false} />
         </div>
         ))}
         {showInner && outlineWheel.map((dice, i) => (
@@ -292,32 +190,23 @@ function App() {
           <Dice {...dice} />
         </div>
       ))}
-        {/* <img className="w-10 h-[40rem]"src="/images/ArrowVertical.png" alt="arrow"
-        style={{
-            position: 'absolute',
-            top: '15%',
-            left: '54%',
-          }}/> */}
         {showOuter && <WheelOutline radius={outerRadius} padding={64} />}
         {showInner && <WheelOutline radius={letterRadius} padding={46} />}
         <WheelOutline radius={6} padding={2} />
   {/* <WheelOutline radius={innerRadius} /> */}
     </div>
-      {/* {outerWheel.map((dice) => (
-        <Dice {...dice}  key={`${dice.number}${dice.dice}`} />
-      ))}
-      {innerWheel.map((dice) => (
-        <Dice {...dice}  key={`${dice.number}${dice.dice}`} />
-      ))} */}
-      {/* <div className="rounded-full outline outline-black w-[700px] h-[700px]"
-      style={{
-            position: 'absolute',
-            top: '38%',
-            left: '38%',
-          }}></div> */}
       <div className="h-[900px]"></div>
       <div className="flex flex-col items-center w-full">
-      <div className="flex w-full justify-center gap-10">
+        <div className="flex w-full justify-center gap-10">
+            <p className="font-['Bellota'] text-6xl mb-4 ml-8">Show outer wheel</p>
+            <input className="w-12 h-12" type='checkbox' checked={showOuter} onChange={handleShowOuter} />
+            <p className="font-['Bellota'] text-6xl mb-4 ml-8">Show inner wheel</p>
+          <input className="w-12 h-12" type='checkbox' checked={showInner} onChange={handleShowInner} />
+          <p className="font-['Bellota'] text-6xl mb-4 ml-8">Hide not key</p>
+            <input className="w-12 h-12" type='checkbox' checked={hideNotKey} onChange={handleHideNotKey} />
+        </div>
+        <div className="h-[100px]"></div>
+        <div className="flex w-full justify-center gap-10">
         <div className="flex-col flex w-[60rem]">
           <p className="font-['Bellota'] text-6xl mb-4 ml-8">Select your key -</p>
         <Select
@@ -350,12 +239,9 @@ function App() {
           <Dice {...key.value} />
           <p className="font-['Bellota'] text-8xl">|</p>
           {message && [...message].map((char,index) => {
-            console.log(char)
             const foundIndex = letterWheel.findIndex((dice) => dice.number === char);
             const charIndex = foundIndex === -1 ? -1 : (foundIndex + turns) % letterWheel.length;
-            console.log(`charIndex: ${charIndex}`)
             if (foundIndex !== -1) {
-              // console.log(`${outerWheel[charIndex].number}${outerWheel[charIndex].dice}`)
               return (<Dice {...outerWheel[charIndex]} key={`codedMessage${index}`} />)
             }
             else
@@ -370,12 +256,9 @@ function App() {
           <Dice {...innerWheel[variantIndex]} />
           <p className="font-['Bellota'] text-8xl">|</p>
           {message && [...message].map((char,index) => {
-            console.log(char)
             const foundIndex = letterWheel.findIndex((dice) => dice.number === char);
             const charIndex = foundIndex === -1 ? -1 : (foundIndex + variantIndex) % letterWheel.length;
-            console.log(`charIndex: ${charIndex}`)
             if (foundIndex !== -1) {
-              // console.log(`${outerWheel[charIndex].number}${outerWheel[charIndex].dice}`)
               return (<Dice {...outerWheel[charIndex]} key={`codedMessage${index}`} />)
             }
             else
--- client/components/Dice.tsx
+++ client/components/Dice.tsx
@@ -1,14 +1,17 @@
-import { DiceIcon } from "../../models/main";
+interface Props {
+  number: number | string
+  dice: string
+  hideNotKey?: boolean
+}
 
-function Dice({ number, dice }: DiceIcon) {
+
+function Dice({ number, dice, hideNotKey }: Props) {
+  if (hideNotKey) {
+    dice = "Blank"
+    number = ""
+  }
   return (
   <div className="grid place-items-center [grid-template-areas:'overlap']" >
-      {/* <img className="[grid-area:overlap] h-24" src={`/images/${dice}.png`} alt={`${dice}`} />
-      {dice == "D4" && <p className="[grid-area:overlap] font-['Bellota'] text-5xl text-white mt-5 -ml-1">{number}</p>}
-      {dice == "D6" && <p className="[grid-area:overlap] font-['Bellota'] text-5xl text-white mt-2 -ml-3">{number}</p>}
-      {dice == "D10" && <p className="[grid-area:overlap] font-['Bellota'] text-4xl text-white -mt-5">{number}</p>}
-      {dice == "D12" && <p className="[grid-area:overlap] font-['Bellota'] text-4xl text-white">{number}</p>}
-      {dice == "D20" && <p className="[grid-area:overlap] font-['Bellota'] text-3xl text-white mt-3">{number}</p>} */}
       
       {dice == "D4" && <img className="[grid-area:overlap] h-20 ml-1 -mt-1" src={`/images/${dice}.png`} alt={`${dice}`} />}
       {dice == "D6" && <img className="[grid-area:overlap] h-20 -mt-2 ml-3" src={`/images/${dice}.png`} alt={`${dice}`} />}

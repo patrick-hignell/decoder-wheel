77e10eb5a28ace593371aadf2f0cfa90d04e6565
Patrick Hignell <patrickhignell@hotmail.com>
2026-04-10 12:28:08 +1200

    edit dice

--- client/components/Dice.tsx
+++ client/components/Dice.tsx
@@ -4,30 +4,93 @@ interface Props {
   hideNotKey?: boolean
 }
 
-
 function Dice({ number, dice, hideNotKey }: Props) {
   if (hideNotKey) {
-    dice = "Blank"
-    number = ""
+    dice = 'Blank'
+    number = ''
   }
   return (
-  <div className="grid place-items-center [grid-template-areas:'overlap']" >
-      
-      {dice == "D4" && <img className="[grid-area:overlap] h-20 ml-1 -mt-1" src={`/images/${dice}.png`} alt={`${dice}`} />}
-      {dice == "D6" && <img className="[grid-area:overlap] h-20 -mt-2 ml-3" src={`/images/${dice}.png`} alt={`${dice}`} />}
-      {dice == "D10" && <img className="[grid-area:overlap] h-24 mt-2" src={`/images/${dice}.png`} alt={`${dice}`} />}
-      {dice == "D12" && <img className="[grid-area:overlap] h-24" src={`/images/${dice}.png`} alt={`${dice}`} />}
-      {dice == "D20" && <img className="[grid-area:overlap] h-24 -mt-3" src={`/images/${dice}.png`} alt={`${dice}`} />}
-      {dice == "D20Outline" && <img className="[grid-area:overlap] h-[6.75rem] -mt-[0.9rem] -ml-2" src={`/images/${dice}.2.png`} alt={`${dice}`} />}
-      {dice == "Blank" && <img className="[grid-area:overlap] h-24 -mt-3" src={`/images/${dice}.png`} alt={`${dice}`} />}
+    <div className="grid place-items-center [grid-template-areas:'overlap']">
+      {dice == 'D4' && (
+        <img
+          className="-mt-1 ml-1 h-20 [grid-area:overlap]"
+          src={`/images/${dice}.png`}
+          alt={`${dice}`}
+        />
+      )}
+      {dice == 'D6' && (
+        <img
+          className="mb-3 mr-3 h-20 [grid-area:overlap]"
+          src={`/images/${dice}.png`}
+          alt={`${dice}`}
+        />
+      )}
+      {dice == 'D10' && (
+        <img
+          className="mt-2 h-24 [grid-area:overlap]"
+          src={`/images/${dice}.png`}
+          alt={`${dice}`}
+        />
+      )}
+      {dice == 'D12' && (
+        <img
+          className="h-24 [grid-area:overlap]"
+          src={`/images/${dice}.png`}
+          alt={`${dice}`}
+        />
+      )}
+      {dice == 'D20' && (
+        <img
+          className="-mt-3 h-24 [grid-area:overlap]"
+          src={`/images/${dice}.png`}
+          alt={`${dice}`}
+        />
+      )}
+      {dice == 'D20Outline' && (
+        <img
+          className="-ml-2 -mt-[0.9rem] h-[6.75rem] [grid-area:overlap]"
+          src={`/images/${dice}.2.png`}
+          alt={`${dice}`}
+        />
+      )}
+      {dice == 'Blank' && (
+        <img
+          className="-mt-3 h-24 [grid-area:overlap]"
+          src={`/images/${dice}.png`}
+          alt={`${dice}`}
+        />
+      )}
 
-      {dice == "D4" && <p className="[grid-area:overlap] font-['Bellota'] text-4xl mt-4 text-white">{number}</p>}
-      {dice == "D6" && <p className="[grid-area:overlap] font-['Bellota'] text-4xl text-white">{number}</p>}
-      {dice == "D10" && <p className="[grid-area:overlap] font-['Bellota'] text-4xl text-white -mt-3">{number}</p>}
-      {dice == "D12" && <p className="[grid-area:overlap] font-['Bellota'] text-4xl text-white">{number}</p>}
-      {dice == "D20" && <p className="[grid-area:overlap] font-['Bellota'] text-3xl text-white">{number}</p>}
-      {dice == "Blank" && <p className="[grid-area:overlap] font-['Bellota'] text-7xl text-black">{number}</p>}
-      
+      {dice == 'D4' && (
+        <p className="mt-4 font-['Bellota'] text-4xl text-white [grid-area:overlap]">
+          {number}
+        </p>
+      )}
+      {dice == 'D6' && (
+        <p className="mr-5 font-['Bellota'] text-4xl text-white [grid-area:overlap]">
+          {number}
+        </p>
+      )}
+      {dice == 'D10' && (
+        <p className="-mt-3 font-['Bellota'] text-4xl text-white [grid-area:overlap]">
+          {number}
+        </p>
+      )}
+      {dice == 'D12' && (
+        <p className="font-['Bellota'] text-4xl text-white [grid-area:overlap]">
+          {number}
+        </p>
+      )}
+      {dice == 'D20' && (
+        <p className="font-['Bellota'] text-3xl text-white [grid-area:overlap]">
+          {number}
+        </p>
+      )}
+      {dice == 'Blank' && (
+        <p className="font-['Bellota'] text-7xl text-black [grid-area:overlap]">
+          {number}
+        </p>
+      )}
     </div>
   )
 }
